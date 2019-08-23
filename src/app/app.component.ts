db8413de9791f3ebd6ac374b167c0d7c13efc9d2
mpk75234 <mikekennedy75234@gmail.com>
2019-08-23 04:03:36 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import {  map } from 'rxjs/operators';
+import { of, fromEvent, interval, Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-bunnyslope';
+
+  ngOnInit(){
+    document.addEventListener('click', evh =>{
+      console.log("VAN HALEN! " + evh);
+    })
+  }
 }
